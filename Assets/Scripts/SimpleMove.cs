2b89c3338a9c1e298aa8b1c05fc6814abf3539ff
Danc <danc.arturo243@gmail.com>
2019-01-26 13:46:25 -0600

    player animation

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -8,6 +8,9 @@ public class SimpleMove : MonoBehaviour
     public int damage = 1;
     public float RotationSpeed = 1;
     public int gainSanity = 1;
+
+    public Animator animator;
+
     void Start()
     {
         InvokeRepeating("insanity", 1f, time);
@@ -21,26 +24,35 @@ public class SimpleMove : MonoBehaviour
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
+
+        float a = Mathf.Abs(hor) + Mathf.Abs(ver);
+        animator.SetFloat("Speed", a);
+
         Vector3 playerMovement = new Vector3(hor, 0, ver) * 10 * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
 
         hor = Input.GetAxis("Horizontal2");
-        transform.Rotate(0, hor*1.2f, 0);
+        transform.Rotate(0, hor * 1.2f, 0);
     }
 
-    private void insanity(){
+    private void insanity()
+    {
         SanityManager.sharedInstance.sanityIncrease(damage);
     }
-    private void sanity(){
+    private void sanity()
+    {
         SanityManager.sharedInstance.sanityDecrease(gainSanity);
     }
 
-    public void OnTriggerEnter(Collider other) {
-        if(other.tag == "fire"){
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "fire")
+        {
             InvokeRepeating("sanity", 1f, 0.5f);
         }
     }
-    public void OnTriggerExit(Collider other){
+    public void OnTriggerExit(Collider other)
+    {
         if (other.tag == "fire")
         {
             CancelInvoke("sanity");
