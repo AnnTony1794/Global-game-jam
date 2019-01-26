bee6cfd5887aed3376717467136890906a7b570d
AnnTony <antonio.facundo1794@gmail.com>
2019-01-25 22:31:44 -0600

    Added managers scripts and game objects.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    inGame,
+    menu,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    //=========================================================================
+    //Variables
+    //=========================================================================
+    public static GameManager sharedInstance; //Singleton
+    public GameState currentGameState = GameState.menu; //GameState
+
+
+    private void Awake() {
+        //Instanciate singleton
+        if(sharedInstance == null){
+            sharedInstance = this;
+        }    
+    }
+
+
+    //=========================================================================
+    //Unity methods
+    //=========================================================================
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    //=========================================================================
+    //Our methods
+    //=========================================================================
+    
+    private void SetGameState(GameState newGameState)
+    {
+        switch (newGameState)
+        {
+            case GameState.inGame:
+            //TODO: Iniciar o reiniciar la escena.
+            break;
+            case GameState.menu:
+            //TODO: Mostrar menú.
+            break;
+            case GameState.gameOver:
+            //TODO: Lógica de muerte.
+            break;
+        }
+    }
+}
