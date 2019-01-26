91f6684f1c9668168f90c3d918608e433056c45c
Miguel Bonilla <jeff.mov@gmail.com>
2019-01-26 01:46:49 -0600

    Otro mas

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    Vector3 t;
+    public Camera m_Camera;
+    void Start () {
+        t = new Vector3(transform.eulerAngles.x, transform.eulerAngles.x, transform.eulerAngles.z);
+     }
+
+    void LateUpdate() 
+    {
+        //transform.LookAt(Camera.main.transform.position, Vector3.up);
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+        m_Camera.transform.rotation * Vector3.up);
+        transform.eulerAngles = new Vector3 (t.x, t.y, transform.eulerAngles.z - t.x);
+    }
+
+}
---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+
+    float mouseX, mouseY;
+    public float RotationSpeed = 1;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, -ver, 0.0f) * 10 * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * -RotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -30f, 60f);
+        transform.Rotate(mouseX, mouseY, 0);
+        /*
+        transform.LookAt(Target);
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        */
+
+    }
+}
