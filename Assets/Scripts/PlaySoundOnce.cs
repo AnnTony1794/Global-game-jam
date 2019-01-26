119825f64c80ce58f52d16cfc307f6d262ecc688
Danc <danc.arturo243@gmail.com>
2019-01-26 07:51:25 -0600

    374 cambios
    que vergas hiciste?!

---
+++ Assets/Scripts/PlaySoundOnce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnce : MonoBehaviour
+{
+    public AudioClip clip;
+    public float volumen;
+    AudioSource source;
+    private bool wasPlayed = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void onTriggerEnter()
+    {
+        if (!wasPlayed)
+        {
+            source.PlayOneShot(clip, volumen);
+            wasPlayed = true;
+        }
+    }
+}
