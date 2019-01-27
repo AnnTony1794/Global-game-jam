11c96da92ac4c878a9232207cbd3c68fe02ba1b7
Miguel Bonilla <jeff.mov@gmail.com>
2019-01-26 23:17:56 -0600

    Buena suerte
    aaaaa

---
+++ Assets/Scripts/audioHandle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioHandle : MonoBehaviour
+{
+    AudioSource audioData;
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+        //audioData.Play(0);
+        //Debug.Log("started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
