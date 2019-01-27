7f941fb3baa0493289fff7d086e5f5b9a8d9ba22
TacoDeTrompo <rodrguez.samuel@gmail.com>
2019-01-27 08:43:10 -0600

    sprite changes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,17 +7,30 @@ public class SoundManager : MonoBehaviour
 {
     public AudioMixerSnapshot Fogata;
     public AudioMixerSnapshot Fondo;
-    
+    public AudioMixerSnapshot Cordura;
+    private bool fogataza = false;
+    private bool cordu = false;
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "FogataFondo"){
             Fogata.TransitionTo(1.2f);
+            fogataza = true;
+            cordu = false;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if(other.tag == "FogataFondo"){
             Fondo.TransitionTo(1.2f);
+            fogataza = false;
+            cordu = false;
+        }
+    }
+
+    private void Update() {
+        if(SanityManager.sharedInstance.playerSanity > 50 && !fogataza && !cordu){
+            Cordura.TransitionTo(0.01f);
+            cordu = true;
         }
     }
 }
